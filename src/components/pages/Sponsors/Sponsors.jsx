76392f18033dcd6ab51d9ae44a5bb7b8943a22f0
yash2002109 <yash2002109@gmail.com>
2021-11-16 15:40:46 +0530

    Create basic layout of Sponsors Page
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

---
+++ src/components/pages/Sponsors/Sponsors.jsx
@@ -0,0 +1,248 @@
+import Menu from "./SideBar/Menu";
+import "./Sponsors.css";
+import SponsorSection from "./SponsorSection/SponsorSection";
+
+function Sponsors() {
+  return (
+    <div
+    className="wrapper"
+      style={{
+        backgroundImage: `url("https://fmcweekend-angrycder.vercel.app/d.e7966721851bfd18d00b.jpg")`,
+        // width: "100vw",
+        // height: "100vh",
+      }}
+     >
+      <div className="sidebar">
+        <Menu />
+      </div>
+
+
+      <div className="main">
+        
+        <SponsorSection />
+        <h1 id="sponsors">Sponsors</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <h1 id="sponsor_1">Sponsors</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <h1 id="sponsor_2">Sponsors</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <h1 id="sponsor_3">Sponsors</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat omnis
+          praesentium quam cum saepe quia laborum est sapiente sint quidem
+          obcaecati, reiciendis, repudiandae hic voluptatum iste porro illum
+          dolorem ipsum!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors;
