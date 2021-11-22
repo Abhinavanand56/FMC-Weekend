f212672c8441b075c569e8a0144fa62dcd194579
yash2002109 <yash2002109@gmail.com>
2021-11-22 22:08:11 +0530

    Add link to cards and correct card text color
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

--- src/components/pages/Sponsors/SponsorSection/Card.jsx
+++ src/components/pages/Sponsors/SponsorSection/Card.jsx
@@ -7,14 +7,18 @@ import image_2 from "./logo.jpg";
 function Card(props) {
   return (
     <div className={classes.parent_wrapper}>
+      <a href="https://www.google.com/">
       <div className={classes.card}>
+        
         <img src={image_2} alt="Avatar" style={{ width: "100%" }} />
         <div className={classes.container}>
           {/* <h4><b>John Doe</b></h4> */}
           {/* <p>Architect  Engineer</p> */}
           <h1>Media Sponsor</h1>
         </div>
+        
       </div>
+      </a>
     </div>
   );
 }
