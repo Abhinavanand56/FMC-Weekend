8554be4be88c3840a537a3d0157963d65603f2de
yash2002109 <yash2002109@gmail.com>
2021-12-21 15:50:46 +0530

    Create FAQ Page
    Co-authored-by: Khushi Kapoor <khushi.kapoor.cd.eee20@itbhu.ac.in>
    Signed-off-by: yash2002109 <yash2002109@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar"
+import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
-import SignUp from './components/pages/Sign-up/SignUp'
-import events from './components/pages/Events/events'
+import SignUp from './components/pages/Sign-up/SignUp';
+import events from './components/pages/Events/events';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Sponsors from "./components/pages/Sponsors/Sponsors";
-
+import Sponsors from './components/pages/Sponsors/Sponsors';
+import FAQ from './components/pages/FAQ/FAQ';
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/sign-up' component={SignUp} />;
-          <Route path='/events' component={events} />;
-          <Route path='/sponsors' component={Sponsors} />
+          <Route path="/" exact component={Home} />
+          <Route path="/sign-up" component={SignUp} />;
+          <Route path="/events" component={events} />;
+          <Route path="/sponsors" component={Sponsors} />
+          <Route path="/FAQ" component={FAQ} />
         </Switch>
       </Router>
     </>
